390ea95e1b39bf9be306549456bb398401b315cf
mymoid <cromero@mymoid.com>
2018-09-10 10:34:37 +0200

    Added readme and system out traces

---
+++ README.md
@@ -0,0 +1,8 @@
+# README #
+
+Run test from a class with main method(Main.java)
+
+
+![Main class](/images/main.jpg?raw=true "Main class")
+
+![Test class](/imagestest.jpg?raw=true "Test class")
--- src/test/java/com/c0deattack/cu/runners/Main.java
+++ src/test/java/com/c0deattack/cu/runners/Main.java
@@ -1,15 +1,23 @@
 package com.c0deattack.cu.runners;
 
-import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runners.model.InitializationError;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Running tests!");
 
-        JUnitCore engine = new JUnitCore();
-        engine.addListener(new TextListener(System.out));
-        engine.run(SpringSampleTest.class);
+
+    public static void main(String[] args) {
+        System.out.println("Running tests of SpringSampleTest !");
+        try {
+
+            final Result run = new JUnitCore().run(new SpringJUnit4ClassRunner(SpringSampleTest.class));
+
+        } catch (InitializationError initializationError) {
+            initializationError.printStackTrace();
+        }
+
     }
 }
