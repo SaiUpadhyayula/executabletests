d7f4fb0278289c141aeca944b14966bfa65031a8
taaupsa1 <subramanyam.sai90@gmail.com>
2018-09-10 17:15:13 +0200

    Removed SpringJunit4ClasRunner instantiation inside the Main class

--- src/test/java/com/c0deattack/cu/runners/Main.java
+++ src/test/java/com/c0deattack/cu/runners/Main.java
@@ -2,24 +2,14 @@ package com.c0deattack.cu.runners;
 
 import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
-import org.junit.runner.Result;
-import org.junit.runners.model.InitializationError;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 public class Main {
 
 
-
     public static void main(String[] args) {
         System.out.println("Running tests of SpringSampleTest !");
-        try {
-            JUnitCore jUnitCore = new JUnitCore();
-            jUnitCore.addListener(new TextListener(System.out));
-            jUnitCore.run(new SpringJUnit4ClassRunner(SpringSampleTest.class));
-
-        } catch (InitializationError initializationError) {
-            initializationError.printStackTrace();
-        }
-
+        JUnitCore jUnitCore = new JUnitCore();
+        jUnitCore.addListener(new TextListener(System.out));
+        jUnitCore.run(SpringSampleTest.class);
     }
 }
